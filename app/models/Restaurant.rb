cfac1cffbf5cecfe0a3844ae7642ee60880cca23
Sam <skim6281@gmail.com>
2017-08-12 12:36:31 -0400

    Connect to Yelp API

--- app/models/Restaurant.rb
+++ app/models/Restaurant.rb
@@ -2,13 +2,37 @@ class Restaurant
   include HTTPParty
   CLIENT_ID = ENV['client_id']
   CLIENT_SECRET = ENV['client_secret']
-  API_HOST = "https://api.yelp.com"
-  TOKEN_PATH = "/oauth2/token"
-  SEARCH_PATH = "/v3/businesses/search"
-  GRANT_TYPE = "client_credentials"
+  API_HOST = "https://api.yelp.com".freeze
+  TOKEN_PATH = "/oauth2/token".freeze
+  SEARCH_PATH = "/v3/businesses/search".freeze
+  GRANT_TYPE = "client_credentials".freeze
   SEARCH_LIMIT = 20
 
   def bearer_token
-    url = "#{BASE_SEARCH_URL}#{TOKEN_PATH}"
+    url = "#{API_HOST}#{TOKEN_PATH}"
+
+    raise "Please set your CLIENT_ID" if CLIENT_ID.nil?
+    raise "Please set your CLIENT_SECRET" if CLIENT_SECRET.nil?
+
+    params = {
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: GRANT_TYPE
+    }
+
+    response = HTPP.post(url, params: params)
+    parsed = response.parse
+    "#{parsed['token_type']} #{parsed['access_token']}"
+  end
+
+  def search(city)
+    url = "#{API_HOST}#{SEARCH_PATH}"
+    params = {
+      location: city,
+      limit: SEARCH_LIMIT
+    }
+
+    response = HTTP.auth(bearer_token).get(url, params: params)
+    response.parse
   end
 end
