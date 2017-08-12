2b3ec625ee00e7e2fc8233aab3a1c6f02e41af87
Sam <skim6281@gmail.com>
2017-08-12 14:31:18 -0400

    Add show action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,7 @@
 class RestaurantsController < ApplicationController
+  def show
+    restaurant = Restaurant.new
+    @restaurants = restaurant.search(params[:city])
+    render 'restaurants/show'
+  end
 end
