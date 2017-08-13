f783fbd6f7805f031ab7e10e889fc661e27df161
Sam <skim6281@gmail.com>
2017-08-13 17:25:30 -0400

    Render restaurant names

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchRestaurants } from './actions';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.fetchRestaurants('new york');
+  }
+
+  renderRestaurants() {
+    return this.props.restaurants.map((restaurant,ind) => {
+      return (<li key={ind}>{restaurant.name} {restaurant.url}</li>);
+    });
+  }
+
   render() {
     return (
-      <div>MAIN</div>
+      <div>MAIN
+      <ul>{this.renderRestaurants()}</ul></div>
     )
   }
 }
 
+export const mapStateToProps = state => {
+  return {
+    restaurants: state.restaurants
+  }
+}
 
-export default Main;
+export const mapDispatchToProps = dispatch => {
+  return {
+    fetchRestaurants: city => dispatch(fetchRestaurants(city))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Main);
